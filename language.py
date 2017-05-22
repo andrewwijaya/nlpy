875c6de5b1e4240d138ac5e74b810f6a00d681cb
Andrew Wijaya <andrew.a.wijaya@gmail.com>
2017-05-22 10:28:58 +0100

    restructured directory, refactored code

--- language.py
+++ language.py
@@ -1,23 +1,25 @@
+"""
+This NLP program takes a piece of text from a file and outputs the 30 bigrams of the highest
+frequency from the text. File path is hardcoded and needs to be amended directly.
+"""
+
 import nltk
 from nltk.corpus import stopwords
 from string import punctuation
 from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.collocations import *
-import pprint as pp
-import matplotlib
 
-#Takes text from a string
-#Work to be done to make it import from a text
-file_object = open("crawler_out_raw.txt")
+#Takes text from a file
+file_object = open("Example text/crawler_out_raw.txt")
 text_article = ''.join(file_object.readlines()).replace('\n',"")
 
 #Print text file after removing carriage returns
-with open("article_out.txt", "w+") as my_file:
+with open("Latest output/article_out.txt", "w+") as my_file:
     my_file.write(text_article)
 
 #Print text file after stripping white space
 text_article_nowhite = text_article.rstrip()
-with open("article_out_nowhite.txt", "w+") as my_file:
+with open("Latest output/article_out_nowhite.txt", "w+") as my_file:
     my_file.write(text_article_nowhite)
 
 #Get stopwords from nltk.corpus module
@@ -25,7 +27,6 @@ punc_list = list(punctuation)
 punc_list.append("''")
 punc_list.append("``")
 customStopWords = set(stopwords.words('english') + punc_list)
-print(punctuation)
 
 #Create a new string for text excluding stopwords
 words_stopword_less = [word for word in word_tokenize(text_article) if word not in customStopWords]
@@ -35,7 +36,7 @@ bigram_measures = nltk.collocations.BigramAssocMeasures()
 finder = BigramCollocationFinder.from_words(words_stopword_less)
 bigrams = sorted(finder.ngram_fd.items(), key=lambda tuplet:tuplet[1], reverse=True)
 
-with open("article_bigrams.txt", "w+") as my_file:
+with open("Latest output/article_bigrams.txt", "w+") as my_file:
     my_file.write(str(bigrams))
 
 for bigram in bigrams[:30]:
